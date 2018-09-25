8c7bdbb1c2d548f5cbb30b09db702162f3a201a9
Lisa Fischer <lancino@web.de>
2018-09-25 15:28:35 +0200

    converted assertion to tests

--- tests.py
+++ tests.py
@@ -1,10 +1,13 @@
 from  dna import DNA
-try:
-    assert DNA('ATB')
-except ValueError:
-    pass
-assert DNA('GTC').complimentary_sequence == DNA('CAG')
-assert DNA('ATC').complimentary_sequence == DNA('TAG')
-assert DNA('ATC').complimentary_sequence == 'TAG'
+import pytest
+def test_bad_sequence_raises_error():
+
+    with pytest.raises(ValueError):
+        DNA('ATB')
+def test_bad_sequence_raises_works():
+
+     assert DNA('GTC').complimentary_sequence == DNA('CAG')
+     assert DNA('ATC').complimentary_sequence == DNA('TAG')
+
 
 print('it worked')
